ccf04a1672aaf4201c948d5b07ac440c41ff5861
H.M.E <1316126217@qq.com>
2022-05-22 15:49:32 +0800

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-
 // import logo from './logo.svg'
-import { IconStyle } from './assets/iconfont/iconfont';
-import { GlobalStyle } from './style';
-import  MyRouter from './router/index'
+import { IconStyle } from "./assets/iconfont/iconfont";
+import { GlobalStyle } from "./style";
+import MyRouter from "./router/index";
+import store from "./store/index";
+import { Provider } from 'react-redux';
 function App() {
-
   return (
-    <div className="App">
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      {/* <i className="iconfont">&#xe62b;</i> */}
-      <MyRouter></MyRouter>
-
-    </div>
-  )
+    <Provider store={store}>
+      <div className="App">
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {/* <i className="iconfont">&#xe62b;</i> */}
+        <MyRouter></MyRouter>
+      </div>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
