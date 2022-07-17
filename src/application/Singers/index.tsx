3f2da88c3b73b579d1a2a8c6f1cb918bc183fbdf
H.M.E <1316126217@qq.com>
2022-07-17 18:08:02 +0800

    feat:添加歌手页面滑动栏

--- src/application/Singers/index.tsx
+++ src/application/Singers/index.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Horizen from '@/baseUI/horizen-item/index';
+import { categoryTypes, alphaTypes } from '@/api/config';
+import { NavContainer } from './style';
 
 function Singers(props) {
-  return  (
-    <div>Singers</div>
-  )
+  let [category, setCategory] = useState('');
+  let [alpha, setAlpha] = useState('');
+  let handleUpdateAlpha = (val) => {
+    setAlpha(val);
+  };
+  let handleUpdateCategory = (val) => {
+    setCategory(val);
+  };
+  return (
+    <NavContainer>
+      <Horizen
+        list={categoryTypes}
+        title={'分类(默认热门):'}
+        handleClick={handleUpdateCategory}
+        oldVal={category}
+      />
+      <Horizen
+        list={alphaTypes}
+        title={'首字母:'}
+        handleClick={handleUpdateAlpha}
+        oldVal={alpha}
+      />
+    </NavContainer>
+  );
 }
 
 export default React.memo(Singers);
