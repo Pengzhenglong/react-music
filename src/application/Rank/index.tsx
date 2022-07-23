acf92997d2a624b59f316bd99dda8918a041550e
H.M.E <1316126217@qq.com>
2022-07-23 19:07:31 +0800

    feat:添加排行榜页面

--- src/application/Rank/index.tsx
+++ src/application/Rank/index.tsx
@@ -1,9 +1,103 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getRankList } from './store/index';
+import Loading from '@/baseUI/loading';
+import { List, ListItem, SongList, Container } from './style';
+import Scroll from '@/baseUI/scroll/index';
+import { EnterLoading } from '@/application/Singers/style';
+import { filterIndex, filterIdx } from '@/api/utils';
+import { Outlet } from 'react-router-dom';
 function Rank(props) {
-  return  (
-    <div>Rank</div>
-  )
-}
+  const { rankList: list, loading } = props;
+  const { getRankListDataDispatch } = props;
 
-export default React.memo(Rank);
+  let rankList = list ? list.toJS() : [];
+  console.log('rankList', rankList);
+  useEffect(() => {
+    if (!rankList.length) {
+      getRankListDataDispatch();
+    }
+  }, []);
+  let globalStartIndex = filterIndex(rankList);
+  let officialList = rankList.slice(0, globalStartIndex);
+  let globalList = rankList.slice(globalStartIndex);
+
+  const enterDetail = (name) => {
+    const idx = filterIdx(name);
+    if (idx === null) {
+      alert('暂无相关数据');
+      return;
+    }
+  };
+
+  const renderSongList = (list) => {
+    return list.length ? (
+      <SongList>
+        {list.map((item, index) => {
+          return (
+            <li key={index}>
+              {index + 1}.{item.first}-{item.second}
+            </li>
+          );
+        })}
+      </SongList>
+    ) : null;
+  };
+
+  const renderRankList = (list, global) => {
+    return (
+      <List globalRank={global}>
+        {list.map((item) => {
+          return (
+            <ListItem
+              key={item.coverImgId}
+              tracks={item.tracks}
+              onClick={() => enterDetail(item.name)}
+            >
+              <div className="img_wrapper">
+                <img src={item.coverImgUrl} alt="" />
+                <div className="decorate"></div>
+                <span className="update_frequecy">{item.updateFrequency}</span>
+              </div>
+              {renderSongList(item.tracks)}
+            </ListItem>
+          );
+        })}
+      </List>
+    );
+  };
+  let displayStyle = loading ? { display: 'none' } : { display: 'block' };
+  return (
+    <Container>
+      <Scroll>
+        <div>
+          <h1 className="offical" style={displayStyle}>
+            官方榜
+          </h1>
+          {renderRankList(officialList)}
+          <h1 className="global" style={displayStyle}>
+            全球榜
+          </h1>
+          {renderRankList(globalList, true)}
+          {loading  ?<EnterLoading><Loading></Loading></EnterLoading>:null}
+        </div>
+      </Scroll>
+      <Outlet />
+    </Container>
+  );
+}
+// 映射Redux全局的state到组件的props上
+const mapStateToProps = (state) => ({
+  rankList: state.getIn(['rank', 'rankList']),
+  loading: state.getIn(['rank', 'loading']),
+});
+// 映射dispatch到props上
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getRankListDataDispatch() {
+      dispatch(getRankList());
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Rank));
