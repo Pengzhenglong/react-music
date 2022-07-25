ab0cfb60bb778b3aea1dd65ab2104311153faebc
彭正龙 <pengzhenglong@gridsum.com>
2022-07-25 10:12:54 +0800

    feat: 添加动画和header

--- src/application/Album/index.tsx
+++ src/application/Album/index.tsx
@@ -1,12 +1,32 @@
-import React, { memo } from 'react'
+import React, {useState} from 'react';
 import {Container} from './style';
-
-const Album = memo(() => {
+import { CSSTransition } from 'react-transition-group';
+import { useNavigate, Navigate } from 'react-router-dom';
+import  Header  from '@/baseUI/header/index';
+function Album (props) {
+  const [showStatus, setShowStatus] = useState (true);
+  const navigate = useNavigate();
+  const  goBack = () => {
+    navigate(-1);
+  }
+  const handleBack = () => {
+    setShowStatus (false);
+  };
+  
   return (
-    <Container>
-      1111
-    </Container>
+    <CSSTransition
+      in={showStatus}  
+      timeout={300} 
+      classNames="fly" 
+      appear={true} 
+      unmountOnExit
+      onExited={goBack}
+    >
+      <Container>
+      <Header title={"返回"} handleClick={handleBack}></Header>
+      </Container>
+    </CSSTransition>
   )
-})
+}
 
-export default Album
+export default React.memo (Album);
